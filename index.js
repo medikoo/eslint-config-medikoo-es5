76629dc0cc323d01fbdbc3cc463e582a5ac76bfc
Mariusz Nowak <medyk@medikoo.com>
2017-04-13 12:57:32 +0200

    feat: turn off no-template-curly-in-string ES6 only rule

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = {
 	extends: "eslint:all",
 	env: { commonjs: true },
 	rules: assign({}, require("eslint-config-medikoo").rules, {
+		"no-template-curly-in-string": "off",
 		"prefer-arrow-callback": "off",
 		"prefer-destructuring": "off",
 		"prefer-template": "off"
